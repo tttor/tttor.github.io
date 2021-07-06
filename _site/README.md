168d557512aa1a035a7fb35ad9636c61132d9386
Vektor Dewanto <vektor.dewanto@gmail.com>
2021-07-06 19:03:09 +1000

    fix

--- _site/README.md
+++
@@ -1,2 +0,0 @@
-# Personal Academic Website
-A skeleton-site for a personal academic website, written in Jekyll for hosting with GitHub Pages.
--- push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo '=== commiting ==='
-git pull origin master
-git add --all
-git commit -a -m fix
-
-echo '=== github.com ==='
-git push upstream master
